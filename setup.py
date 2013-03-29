264786f8d17ac7d3f982ef2509d3300985fab9d2
Matthew Rocklin <mrocklin@gmail.com>
2013-03-29 14:04:02 -0500

    fix setup.py - include strategies.branch package

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from os.path import exists
 from setuptools import setup
 
 setup(name='strategies',
-      version='0.1.1',
+      version='0.1.2',
       description='Strategic Programming in python',
       url='http://github.com/logpy/strategies',
       author='Matthew Rocklin',
       author_email='mrocklin@gmail.com',
       license='BSD',
-      packages=['strategies'],
+      packages=['strategies', 'strategies.branch'],
       long_description=open('README.md').read() if exists("README.md") else "",
       zip_safe=False)
