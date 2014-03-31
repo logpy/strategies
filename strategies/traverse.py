b27e0abde84693ffe5faab33e9cc7a6ad0b32990
Matthew Rocklin <mrocklin@gmail.com>
2014-03-30 23:29:30 -0700

    curry traverse

--- strategies/traverse.py
+++ strategies/traverse.py
@@ -1,34 +1,38 @@
 """ Strategies to Traverse a Tree """
 from strategies.core import chain, do_one
 from term import new, op, args, isleaf
+from toolz import curry
 
-def top_down(rule):
+@curry
+def top_down(rule, x):
     """ Apply a rule down a tree running it on the top nodes first """
-    return chain(rule, lambda expr: sall(top_down(rule))(expr))
+    return chain(rule, lambda expr: sall(top_down(rule))(expr))(x)
 
-def bottom_up(rule):
+@curry
+def bottom_up(rule, x):
     """ Apply a rule down a tree running it on the bottom nodes first """
-    return chain(lambda expr: sall(bottom_up(rule))(expr), rule)
+    return chain(lambda expr: sall(bottom_up(rule))(expr), rule)(x)
 
-def top_down_once(rule):
+@curry
+def top_down_once(rule, x):
     """ Apply a rule down a tree - stop on success """
-    return do_one(rule, lambda expr: sall(top_down(rule))(expr))
+    return do_one(rule, lambda expr: sall(top_down(rule))(expr))(x)
 
-def bottom_up_once(rule):
+@curry
+def bottom_up_once(rule, x):
     """ Apply a rule up a tree - stop on success """
-    return do_one(lambda expr: sall(bottom_up(rule))(expr), rule)
+    return do_one(lambda expr: sall(bottom_up(rule))(expr), rule)(x)
 
-def sall(rule):
+@curry
+def sall(rule, expr):
     """ Strategic all - apply rule to args """
-    def all_rl(expr):
-        if isleaf(expr):
-            return expr
-        else:
-            children = args(expr)
-            if isinstance(children, (tuple, list)):
-                children = map(rule, children)
-            if isinstance(children, dict):
-                children = dict(zip(children.keys(),
-                                map(sall(rule), children.values())))
-            return new(op(expr), children)
-    return all_rl
+    if isleaf(expr):
+        return expr
+    else:
+        children = args(expr)
+        if isinstance(children, (tuple, list)):
+            children = map(rule, children)
+        if isinstance(children, dict):
+            children = dict(zip(children.keys(),
+                            map(sall(rule), children.values())))
+        return new(op(expr), children)
